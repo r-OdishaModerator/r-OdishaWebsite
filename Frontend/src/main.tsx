a7798f743e1c3d751ab614454d52024507026845
amiyakt <amiyakt.dev@gmail.com>
2023-04-02 20:18:41 +0530

    sample home page design

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App'
-import './index.css'
-
+import App from './App';
+import './index.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import './styles/Home.css';
 
 const fetchSubRedditData = async () => {
@@ -19,13 +18,38 @@ function Home() {
 			setSubsCnt(subscribers);
 			setActiveUserCnt(active_user_count);
 		});
-	});
+	}, []);
 
 	return (
 		<section className="home">
-			{JSON.stringify(useLocation())}
-			<h3>subscriberCnt: {subscriberCnt}</h3>
-			<h3>activeUserCnt: {activeUserCnt}</h3>
+			<h1 className="subreddit-name">
+				r/<i className="fa-solid fa-dharmachakra"></i>DISHA
+			</h1>
+			<div className="actions-container">
+				<h2>Connect with the community</h2>
+				<div className="actions-btn-group">
+					<button className="btn btn-discord" type='button'>
+						<i className='fa-brands fa-discord discord-icon'></i>
+						Discord
+					</button>
+					<button className="btn btn-reddit" type='button'>
+						<i className='fa-brands fa-reddit-alien reddit-icon'></i>
+						Reddit
+					</button>
+				</div>
+			</div>
+
+			<h4 className="subreddit-stats">
+				{activeUserCnt}/{subscriberCnt} users currently active{' '}
+				<a
+					href="https://www.reddit.com/r/Odisha/"
+					target="_blank"
+					rel="noreferrer"
+					className="subreddit-link"
+				>
+					@reddit
+				</a>
+			</h4>
 		</section>
 	);
 }
